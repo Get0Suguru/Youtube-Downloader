9619c741a8940a8b63faca31f44adc2afb33fd26
Gautam Taneja <gautam45taneja@gmail.com>
2025-07-04 21:32:45 +0530

    Create main Spring Boot application class

---
+++ src/main/java/com/suguru/geto/yt_downloader/YoutubeDowloaderApplication.java
@@ -0,0 +1,13 @@
+package com.suguru.geto.yt_downloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeDowloaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeDowloaderApplication.class, args);
+	}
+
+}
