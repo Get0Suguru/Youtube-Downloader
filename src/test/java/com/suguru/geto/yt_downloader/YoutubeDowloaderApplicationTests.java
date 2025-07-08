f83d41d1f5cc41b037bf2c5e9926e9a667624fc0
Gautam Taneja <gautam45taneja@gmail.com>
2025-07-08 21:05:55 +0530

    Add basic test structure

---
+++ src/test/java/com/suguru/geto/yt_downloader/YoutubeDowloaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suguru.geto.yt_downloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeDowloaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
